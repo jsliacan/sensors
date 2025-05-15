bac02f5162913e14a6604451531579132029c7f7
Jakub Sliacan <jsliacan@proton.me>
2025-05-15 22:12:55 +0200

    Add `use_worker_thread` argument to button
    We introduced worker thread for use with some sensors. No need for it in
    case of the button.

--- button.py
+++ button.py
@@ -22,6 +22,7 @@
 """
 
 import argparse
+import asyncio
 import logging
 import datetime, time
 
@@ -31,13 +32,16 @@ from BicycleSensor import BicycleSensor, configure_logging
 
 class ButtonSensor(BicycleSensor):
 
-  def __init__(self, name, hash, measurement_frequency, upload_interval):
-    BicycleSensor.__init__(self, name, hash, measurement_frequency, upload_interval)
+  def __init__(self, name, hash, measurement_frequency, upload_interval, use_worker_thread):
+    BicycleSensor.__init__(self, name, hash, measurement_frequency, upload_interval, use_worker_thread)
     
     self.PIN = 22                                                 # use GPIO 22
 
     GPIO.setmode(GPIO.BCM)                                        # use GPIO numbering
     GPIO.setup(self.PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)     # set GPIO 22 as input
+
+  async def worker_main(self):
+      pass 
 
   def write_header(self):
     '''Override to write the header to the CSV file.'''
@@ -65,10 +69,11 @@ if __name__ == '__main__':
   PARSER.add_argument('--measurement-frequency', type=float, default=50.0, help='Frequency of sensor measurements in 1/s')
   PARSER.add_argument('--stdout', action='store_true', help='Enables logging to stdout')
   PARSER.add_argument('--upload-interval', type=float, default=5.0, help='Interval between uploads in seconds')
+  PARSER.add_argument('--use_worker_thread', type=bool, default=False, help='Use a background thread for worker process or not')
   ARGS = PARSER.parse_args()
 
   # Configure logging
   configure_logging(stdout=ARGS.stdout, rotating=True, loglevel=ARGS.loglevel, logfile="VTIButton.log")
 
-  button_sensor = ButtonSensor(ARGS.name, ARGS.hash, ARGS.measurement_frequency, ARGS.upload_interval)
+  button_sensor = ButtonSensor(ARGS.name, ARGS.hash, ARGS.measurement_frequency, ARGS.upload_interval, ARGS.use_worker_thread)
   button_sensor.main()
